f12e7b7fb493eb2a446fe4fb2f9f622c4c4277ed
Ezefavourclear <fred>
2024-10-08 14:38:57 +0100

    first commit

---
+++ random.js
@@ -0,0 +1,25 @@
+const passwordBox = document.getElementById("password");
+const lenght = 15;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol = "@#$_&+()^()<>-=";
+const allClass = upperCase + lowerCase + number + symbol;
+
+function createPassword() {
+  let password = "";
+  password += upperCase[Math.floor(Math.random() * upperCase.length)];
+  password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+  password += number[Math.floor(Math.random() * number.length)];
+  password += symbol[Math.floor(Math.random() * symbol.length)];
+
+  while (lenght > password.length) {
+    password += allClass[Math.floor(Math.random() * allClass.length)];
+  }
+  passwordBox.value = password;
+}
+function copyPassword() {
+  passwordBox.select();
+  document.execCommand("copy");
+}
